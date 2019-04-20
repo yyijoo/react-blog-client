6b98e987a91016bc96ee63e0532ebec564e22dcc
yyijoo <yyijoo@gmail.com>
2019-04-21 08:02:55 +0900

    TocPopover : Add style and Render it by clicking in Til component

--- src/components/til/TocPopover.js
+++ src/components/til/TocPopover.js
@@ -1,16 +1,46 @@
 import React, { Component, Fragment } from "react";
-import styeld from "styled-components";
+import styled from "styled-components";
+
+const PopoverWrapper = styled.div`
+  display: inline-block;
+  background: #f2f2f2;
+  margin-top: 0.5rem;
+  padding: 1rem 1rem 1rem 1rem;
+  font-size: 0.8rem;
+  position: relative;
+`;
+
+const content = content => (
+  <PopoverWrapper>
+    {content.map(con => (
+      <div>{con}</div>
+    ))}
+  </PopoverWrapper>
+);
 
 class TocPopover extends Component {
   state = {
     visible: false
   };
 
+  onClickVisible = () => {
+    this.setState({
+      visible: !this.state.visible
+    });
+  };
+
   render() {
+    console.log(this.state.visible);
     return (
       <Fragment>
-        <span style={{ fontSize: "0.8rem" }}>TOC</span>
-        <div>test</div>
+        <div style={{ fontSize: "0.8rem" }} onClick={this.onClickVisible}>
+          TOC
+        </div>
+        {this.state.visible ? (
+          <div>{content(this.props.content) || "no data"}</div>
+        ) : (
+          ""
+        )}
       </Fragment>
     );
   }
