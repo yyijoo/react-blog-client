9c5d440797fbfe1547db72385049fc397e02115b
yyijoo <yyijoo@gmail.com>
2019-04-23 18:49:58 +0900

    App : Add til, resume router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "components/shared/Header";
 import Error from "components/shared/Error";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Til from "pages/til/Til";
+import Resume from "pages/resume/Resume";
 
 class App extends Component {
   render() {
@@ -11,7 +12,8 @@ class App extends Component {
         <HashRouter>
           <Header />
           <Switch>
-            <Route exact path="/" component={Til} />
+            <Route exact path="/til" component={Til} />
+            <Route exact path="/resume" component={Resume} />
             <Route component={Error} />
           </Switch>
         </HashRouter>
