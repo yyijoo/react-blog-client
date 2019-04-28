4c209a879b5f7b54011e329ebeaf7698e416e3f2
yyijoo <yyijoo@gmail.com>
2019-04-28 14:51:06 +0900

    Portfolio : Add projects data for portfolio

---
+++ src/data/portfolioData.js
@@ -0,0 +1,24 @@
+import codestatesWebsite from "data/image/codestatesWebsite.png";
+
+const projectAsEngineer = [
+  {
+    title: "Codestates website",
+    term: "2019.02 - 2019.03",
+    imgURL: codestatesWebsite,
+    link: ""
+  },
+  {
+    title: "Codestates website",
+    term: "2019.02 - 2019.03",
+    imgURL: codestatesWebsite,
+    link: ""
+  },
+  {
+    title: "Codestates website",
+    term: "2019.02 - 2019.03",
+    imgURL: codestatesWebsite,
+    link: ""
+  }
+];
+
+export default projectAsEngineer;
