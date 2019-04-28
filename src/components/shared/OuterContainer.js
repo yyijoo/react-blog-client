5b23a7ebe21b2a40633b0a4daed599e295938d6d
yyijoo <yyijoo@gmail.com>
2019-04-28 14:53:01 +0900

    Make NarrowedContainer

---
+++ src/components/shared/OuterContainer.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const NarrowedContainer = styled.div`
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 8rem;
+`;
+
+export default NarrowedContainer;
