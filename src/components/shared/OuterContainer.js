a9c6681e87a8eccc35646d7d7d932d95c09c9013
yyijoo <yyijoo@gmail.com>
2019-04-30 16:23:09 +0900

    OuterContainer : Make width changeable by props

--- src/components/shared/OuterContainer.js
+++ src/components/shared/OuterContainer.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const NarrowedContainer = styled.div`
-  width: 70%;
+  width: ${props => (props.width ? props.width : "70%")};
   margin-left: auto;
   margin-right: auto;
   margin-top: 8rem;
