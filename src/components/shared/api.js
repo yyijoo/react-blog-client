6b2b6e7eaf240a61827a1eb03ad61b80126b28c2
yyijoo <yyijoo@gmail.com>
2019-05-04 01:35:13 +0900

    Make api

---
+++ src/components/shared/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const local = "http://localhost:8080/api";
+
+export const fetchAllTil = dispatch => axios.get(`${local}/til`);
+
+export const searchTil = values =>
+  axios.get(`${local}/til/search`, { params: values });
+
+export const search = values => {
+  console.log("keyword", values);
+  console.log("here", `${local}/til/search`);
+  axios.get(`${local}/til/search`, { params: values }).then(res => {
+    // this.setState({ data: res.data });
+    console.log("hi", res);
+    return res;
+  });
+};
+
+// http://localhost:8080/api/til/search
