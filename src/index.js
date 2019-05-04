837122fdebce63100e680fbb6d051d0ddd862a98
yyijoo <yyijoo@gmail.com>
2019-05-04 19:11:35 +0900

    Redux : Separate store from index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "App.js";
 import "normalize.css";
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import { reducer as formReducer } from "redux-form";
 import { Provider } from "react-redux";
-import ReduxThunk from "redux-thunk";
-import rootReducer from "redux/reducer";
-
-const store = createStore(
-  rootReducer,
-  compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(ReduxThunk)
-  )
-);
+import store from "redux/store.js";
 
 ReactDOM.render(
   <Provider store={store}>
