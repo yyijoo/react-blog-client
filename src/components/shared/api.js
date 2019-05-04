0770af022d184a482dceea40e469a39952588bf4
yyijoo <yyijoo@gmail.com>
2019-05-04 19:40:26 +0900

    Make api - getting all til data

--- src/components/shared/api.js
+++ src/components/shared/api.js
@@ -3,17 +3,17 @@ const local = "http://localhost:8080/api";
 
 export const fetchAllTil = () => axios.get(`${local}/til`);
 
-export const searchTil = values =>
-  axios.get(`${local}/til/search`, { params: values });
+// export const searchTil = values =>
+//   axios.get(`${local}/til/search`, { params: values });
 
-export const search = values => {
-  console.log("keyword", values);
-  console.log("here", `${local}/til/search`);
-  axios.get(`${local}/til/search`, { params: values }).then(res => {
-    // this.setState({ data: res.data });
-    console.log("hi", res);
-    return res;
-  });
-};
+// export const search = values => {
+//   console.log("keyword", values);
+//   console.log("here", `${local}/til/search`);
+//   axios.get(`${local}/til/search`, { params: values }).then(res => {
+//     // this.setState({ data: res.data });
+//     console.log("hi", res);
+//     return res;
+//   });
+// };
 
 // http://localhost:8080/api/til/search
