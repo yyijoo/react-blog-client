7e299fc96352cf3b56a8ada4c089e6420782f272
yyijoo <yyijoo@gmail.com>
2019-05-07 13:47:10 +0900

    Redux : Change til reducer name

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
-import tilReducer from "redux/reducer/til.js";
+import tilReducer from "redux/reducer/tilReducer.js";
 
 const rootReducer = combineReducers({
   formReducer,
