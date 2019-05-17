bce1fa377e8a80435013efd402e146d01c17dd4c
yyijoo <yyijoo@gmail.com>
2019-05-17 15:21:30 +0900

    Portfolio/ProjectList : Put ProjectList inside ProjectWrapper

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,29 +1,10 @@
 import React, { Component } from "react";
-import ProjectItem from "components/portfolio/ProjectItem";
 import ProjectsWrapper from "components/portfolio/ProjectsWrapper";
 import {
   projectAsEngineer,
   projectAsBusinessDeveloper
 } from "data/portfolioData.js";
 import { Wrapper } from "components/shared/OuterContainer";
-
-const returnProjectItem = (
-  title,
-  subtitle,
-  imgUrl,
-  redirectUrl,
-  noBackground,
-  isSelected
-) => (
-  <ProjectItem
-    title={title}
-    subtitle={subtitle}
-    imgUrl={imgUrl}
-    redirectUrl={redirectUrl}
-    noBackground={noBackground}
-    isSelected={isSelected}
-  />
-);
 
 class Portfolio extends Component {
   render() {
@@ -33,13 +14,11 @@ class Portfolio extends Component {
           section="software"
           title="As Software Enginner"
           data={projectAsEngineer}
-          component={returnProjectItem}
         />
         <ProjectsWrapper
           section="business"
           title="As Business Developer"
           data={projectAsBusinessDeveloper}
-          component={returnProjectItem}
           noBackground={true}
         />
       </Wrapper>
