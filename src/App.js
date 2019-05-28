c39b23ad0e1467491ab0d099d0c81418e51feff9
yyijoo <yyijoo@gmail.com>
2019-05-28 14:28:25 +0900

    feat: Make Blog component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "pages/about";
 import Til from "pages/til";
 import Resume from "pages/resume";
 import Portfolio from "pages/portfolio";
+import Blog from "pages/Blog";
 import styled from "styled-components";
 import { Provider } from "react-redux";
 import store from "redux/store.js";
@@ -27,7 +28,8 @@ class App extends Component {
                 <Route path="/about" component={About} />
                 <Route path="/til" component={Til} />
                 <Route path="/resume" component={Resume} />
-                <Route path="/portfolio/" component={Portfolio} />
+                <Route path="/portfolio" component={Portfolio} />
+                <Route path="/blog" component={Blog} />
                 <Route component={Error} />
               </Switch>
             </ContentsBelowHeaderContainer>
---
+++ src/pages/blog/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Blog extends Component {
+  render() {
+    return <div>teset</div>;
+  }
+}
+
+export default Blog;
