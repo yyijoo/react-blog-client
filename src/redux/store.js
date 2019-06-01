099ac4dde3ff1ec3133bb56455e693d9dcf2920f
yyijoo <yyijoo@gmail.com>
2019-06-01 16:09:55 +0900

    perf: Render redux devtoonl only when it exists

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,11 @@ import { createStore, compose, applyMiddleware } from "redux";
 import ReduxThunk from "redux-thunk";
 import rootReducer from "redux/reducer";
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(ReduxThunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(ReduxThunk))
 );
 
 export default store;
