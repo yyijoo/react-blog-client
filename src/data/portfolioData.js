f6c128295736163484b8fe7984551ca4e9a4f5ab
yyijoo <yyijoo@gmail.com>
2019-06-03 19:26:20 +0900

    docs : Update resume and portfolio data

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -47,7 +47,7 @@ export const projectAsEngineer = [
 
 export const projectAsBusinessDeveloper = [
   {
-    title: "kakao & kakaomobility",
+    title: "kakao / kakaomobility",
     term: "2016.03 - 2018.05",
     imgUrl: codestatesWebsite,
     redirectUrl: "kakao",
@@ -72,10 +72,10 @@ export const projects = {
       }
     ],
     desc:
-      "파킹스퀘어가 인수되면서 카카오 O2O조직에 합류했습니다. 주차장을 찾고 이용할 수 있는 카카오파킹을 위한 목적조직 파킹TF를 시작으로, 사업기획팀에 소속되어 카카오택시 시승, 신차판매 플랫폼, 카카오드라이버 서비스를 담당했습니다.",
+      "파킹스퀘어가 인수되면서 카카오 O2O조직에 합류했습니다. 주차장 검색 및 이용 서비스를 만드는 파킹TF를 시작으로, 사업기획팀에 소속되어 카카오택시 시승, 신차판매 플랫폼, 카카오드라이버 서비스를 담당했습니다.",
     items: [
       "신규 서비스 핵심 가치 정의",
-      "수익 창출을 위한 business model 기획 및 실행",
+      "수익 창출을 위한 business model 설계 및 실행",
       "시너지를 낼 수 있는 제휴사 발굴 및 파트너십 유지",
       "서비스기획, 개발팀과 business에 필요한 요구사항을 정의 및 우선순위 조정"
     ],
@@ -87,11 +87,11 @@ export const projects = {
     desc:
       "주차 예약 서비스 ‘파크히어'를 만드는 파킹스퀘어에서 시리즈A 투자를 받고 카카오에 인수되기까지 전략기획팀 소속으로 일했습니다.",
     items: [
-      "2년 동안 5명이던 팀이 30명이 되는 과정에서의 팀 빌딩과 암묵지 구조적 전파",
-      "핵심지표 설정과 데이터를 통해 추출한 인사이트를 서비스에 빠르게 반영",
-      "서비스 핵심 가치와 비전을 정의, 장기적 사업 방향성 수립 참여",
-      "브랜딩 작업, 보도자료 작성 등 서비스 핵심 가치 전파",
-      "축제, 차량 관련 서비스, 공연장 제휴 등 유저 확장을 위한 채널 발굴"
+      "핵심지표 설정과 데이터를 통해 인사이트를 추출하고 서비스에 반영",
+      "서비스 핵심 가치와 비전 정의, 장기적 사업 방향성 수립",
+      "축제, 차량 관련 서비스, 공연장 제휴 등 유저 확장을 위한 채널 발굴",
+      "브랜딩 작업, 보도자료 작성 등 서비스 핵심 가치를 외부에 전파",
+      "2년 동안 5명이던 팀이 30명이 되는 과정에서 서비스 핵심 가치를 내부에 전파"
     ],
     pics: [parkhere, psqMember]
   },
@@ -102,7 +102,8 @@ export const projects = {
     items: [
       "Figma를 활용한 서비스 기능 정의 및 레이아웃 기획",
       "React, Redux 기반 웹앱 클라이언트 폴더 및 컴포넌트 구조 설계",
-      "json-server를 활용해 수강생 정보 조회, 수정, 삭제 인터페이스 구현"
+      "json-server를 활용해 수강생 정보 CRUD 인터페이스 구현",
+      "수강생 정보 CRUD REST API 구현"
     ],
     pics: []
   },
@@ -130,13 +131,12 @@ export const projects = {
       "styled-components를 활용한 전체 레이아웃 컴퍼넌트 구조 설계 및 구현",
       "이력서 페이지 인터페이스 구현",
       "Google vision API를 이용한 이미지 텍스트 추출 기능 구현",
-      "scotty를 활용한 AWS S3 클라이언트 앱 배포",
-      "로그인 토큰 처리"
+      "scotty를 활용한 AWS S3 클라이언트 앱 배포"
     ]
   },
   personal: {
     title: "Personal website",
-    desc: "이력 소개 및 TIL, Blog 작성용 개인 포트폴리오 웹사이트",
+    desc: "이력 소개 및 TIL, Blog 작성용 개인 웹사이트",
     urls: [
       {
         text: "figma 기획서",
@@ -150,7 +150,8 @@ export const projects = {
     ],
     items: [
       "figma를 활용한 서비스 기능 정의 및 레이아웃 기획",
-      "markdown file rendering"
+      "React, Redux 기반 웹앱 구조 설계 및 구현",
+      "Styled-components 기반 스타일 구현"
     ]
   },
   sool: {
--- src/data/resumeData.js
+++ src/data/resumeData.js
@@ -3,17 +3,11 @@ export default {
     category: "Technical Skills",
     contents: [
       {
-        itemName: "Strong",
-        items: [
-          "Javascript",
-          "HTML/CSS",
-          "Redux",
-          "styled-componenets",
-          "eslint"
-        ]
+        itemName: "Front-end",
+        items: ["Javascript", "HTML/CSS", "React", "Redux", "styled-components"]
       },
       {
-        itemName: "Experienced",
+        itemName: "Back-end",
         items: ["Node.js", "Express", "sequelize"]
       },
       {
@@ -28,25 +22,29 @@ export default {
       {
         title: "Codestates Admin",
         role: "Front-end Engineer",
-        term: "2019.02 - 2019.03",
+        term: "2019.01 - 2019.03",
         desc:
-          "Full-stack | 통합 수강생 관리, 웹사이트 콘텐츠 관리, 결제 관리를 위한 관리자 페이지",
+          "Codestates 재직 중, 통합 수강생 관리 & 공식 웹사이트 콘텐츠 관리 & 결제 관리를 위한 관리자 웹앱 신규 개발",
         items: [
-          "템플릿 기반 웹앱 기획 및 클라이언트 구조 설계",
-          "수강생 정보 CRUD 인터페이스 및 REST API 구현"
+          "Tech Stack : React, Redux, styled-components, Node.js, Express, Sequelize",
+          "운영 및 마케팅 담당자의 사용성을 고려해 웹앱 기획",
+          "확장성 및 재사용성을 고려해 React & Redux 기반 클라이언트 구조 설계",
+          "수강생 정보 CRUD 인터페이스 구현",
+          "수강생 정보 CRUD REST API 구현"
         ]
       },
       {
         title: "Codestates Website",
         role: "Front-end",
-        term: "2019.02 - 2019.03",
+        term: "2019.01 - 2019.03",
         desc:
-          "Full-stack | 코스 안내, 신청, 결제 및 상담이 이뤄지는 공식 웹사이트",
+          "Codestates 재직 중, 코스 안내 & 신청 & 결제 & 상담이 이뤄지는 공식 웹앱 개발",
         items: [
-          "React, Redux, styled - component를 활용한 통합 수강생 관리 페이지 개발",
+          "Tech Stack : React, Redux, styled-components, now",
           "수강신청, 코스소개, 메인 페이지 UI 개편",
-          "브라우저 사이즈별 반응형 대응",
-          "hotfix 대응 등 유지보수"
+          "할인 쿠폰, 메일링 서비스 추가 신청 등 신규 기능 구현",
+          "static data로 관리되던 정보를 DB화하고 hotfix에 대응하는 등 기존 코드 리팩토링 및 유지 보수",
+          "운영 및 마케팅 담당자들과 asana 및 Scrum Time을 활용해 scrum으로 협업"
         ]
       },
       {
@@ -54,13 +52,14 @@ export default {
         role: "Front-end Engineer",
         term: "2018.12 - 2018.12",
         desc:
-          "Front-end | 통합 수강생 관리, 웹사이트 콘텐츠 관리, 결제 관리를 위한 관리자 페이지",
+          "Codestates 4주 프로젝트로, 여러 곳에서 수집한 채용 정보와 본인의 커리어를 정리하는 웹앱 개발",
         items: [
-          "React & Redux를 사용한 웹앱 구조 설계",
-          "전체 레이아웃 설계 및 구현",
-          "이력서 페이지 구현",
+          "Tech Stack : React, Redux, styled-components",
+          "전체 컴퍼넌트 구조 설계",
+          "이력서 페이지 인터페이스 구현",
+          "styled-components를 활용해 grid 기반 레이아웃 설계 및 구현",
           "Google vision API를 이용한 이미지 텍스트 추출 기능 구현",
-          "AWS S3 클라이언트 앱 배포"
+          "scotty를 활용해 AWS S3 클라이언트 앱 배포"
         ]
       },
       {
@@ -68,11 +67,13 @@ export default {
         role: "Front-end & Back-end Engineer",
         term: "2018.11 - 2018.11",
         desc:
-          "Full-stack | 사진 및 텍스트로 일상을 기록 및 관리하는 온라인 일기 서비스",
+          "Codestates 2주 프로젝트로, 사진 및 텍스트로 일상을 기록 및 관리하는 웹앱 개발",
         items: [
-          "Express & Sequelize로 CRUD",
-          "AWS S3 이미지 업로드 기능",
-          "AWS EC2 서버 앱 배포"
+          "Tech Stack : Node.js, Express, Sequelize",
+          "게시글 CRUD API 구현",
+          "multer, aws-sdk, multer-s3, aws-cli를 활용해 AWS S3 이미지 업로드 기능 구현",
+          "AWS EC2 서버 앱 배포",
+          "Semantic UI를 활용한 레이아웃 구현"
         ]
       }
     ]
@@ -91,22 +92,13 @@ export default {
         ]
       },
       {
-        title: "카카오모빌리티",
-        term: "2017.08 - 2018.05 / 10개월",
-        desc: "사업기획팀 소속",
+        title: "카카오 / 카카오모빌리티",
+        term: "2016.03 - 2018.05 / 2년 2개월",
+        desc: "파킹TF / 사업기획팀 소속",
         items: [
           "카카오드라이버 담당. 공급자 확장 전략 수립 및 실행",
-          "신규 온라인 신차 판매 플랫폼 담당.초기 서비스 및 BM 설계와 신규 제휴사 발굴"
-        ]
-      },
-      {
-        title: "카카오",
-        term: "2016.03 - 2017.08 / 1년 6개월",
-        desc: "파킹TF 소속",
-        items: [
-          "신규 온라인 신차 판매 플랫폼 담당. 초기 서비스 설계 및 제휴 계약",
-          "택시 시승 서비스 담당.제휴사 발굴 및 타겟팅 시승 상품 개발",
-          "파킹TF 담당.초기 BM 설계와 공급자 확장 전략 수립 및 실행"
+          "신규 온라인 신차 판매 & 시승 플랫폼 담당. 초기 서비스 설계 및 제휴 계약",
+          "파킹TF 담당. 초기 서비스 설계와 공급자 확장 전략 수립 및 실행"
         ]
       },
       {
@@ -126,9 +118,10 @@ export default {
     contents: [
       {
         title: "Code States",
-        term: "2018.10 - 2018.12 ",
+        term: "2018.07 - 2018.12",
         // items: ["Advanced Software Engineering Immersive Program"]
-        desc: "Advanced Software Engineering Immersive Program 수료"
+        desc:
+          "Pre Course & Advanced Software Engineering Immersive Program 수료"
       },
       {
         title: "성균관대학교 ",
