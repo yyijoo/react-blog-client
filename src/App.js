45d9799aae835d55a33f97ad02d8dc509d729668
yyijoo <yyijoo@gmail.com>
2019-06-06 14:15:30 +0900

    feat: Add Route rendering articleDetail for blog

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Blog from "pages/Blog";
 import styled from "styled-components";
 import { Provider } from "react-redux";
 import store from "redux/store.js";
+import articleDetail from "pages/blog/articleDetail";
 
 const ContentsBelowHeaderContainer = styled.div`
   margin-top: 62px;
@@ -28,8 +29,9 @@ class App extends Component {
                 <Route path="/about" component={About} />
                 <Route path="/til" component={Til} />
                 <Route path="/resume" component={Resume} />
-                <Route path="/portfolio/" component={Portfolio} />
-                <Route path="/blog/" component={Blog} />
+                <Route path="/portfolio" component={Portfolio} />
+                <Route exact path="/blog" component={Blog} />
+                <Route path="/blog/:_id" component={articleDetail} />
                 <Route component={Error} />
               </Switch>
             </ContentsBelowHeaderContainer>
